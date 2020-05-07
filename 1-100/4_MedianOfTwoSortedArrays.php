a7c4633c67732b5e99fe656519e8b53625a9a59d
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-07 17:18:00 +0900

    Update 4_MedianOfTwoSortedArrays.php

--- 1-100/4_MedianOfTwoSortedArrays.php
+++ 1-100/4_MedianOfTwoSortedArrays.php
@@ -1,29 +1,42 @@
 class Solution {
 
     /**
-     * @param String $s
-     * @return Integer
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Float
      */
-    function lengthOfLongestSubstring($s) {
-           if (!$s) {
-            return 0;
-        }
-        $strLength = strlen($s);
-        if (1 == $strLength) {
-            return 1;
+    function findMedianSortedArrays($nums1, $nums2) {
+        if (!$nums1 && !$nums2) {
+            return;
         }
 
-        $charMap = [];
-        $max     = 0;
+        $len1 = count($nums1);
+        $len2 = count($nums2);
 
-        for ($i = 0, $j = 0; $j < $strLength; $j++) {
-            if (!empty($charMap) && in_array($s[$j], array_keys($charMap))) {
-                $i = max($charMap[$s[$j]] + 1, $i);
+        $goalIndex = intval(($len1 + $len2 - 1) / 2);
+
+        $i = 0;
+        $j = 0;
+        for ($k = 0; $k < $goalIndex; $k++) {
+            if ($j >= $len2 || ($i < $len1 && $nums1[$i] < $nums2[$j])) {
+                $i++;
+            } else {
+                $j++;
             }
-            $charMap[$s[$j]] = $j;
-            $max             = max($max, $j - $i + 1);
+        }
+        if ($j >= $len2 || ($i < $len1 && $nums1[$i] < $nums2[$j])) {
+            $goalNum = $nums1[$i];
+            $i++;
+        } else {
+            $goalNum = $nums2[$j];
+            $j++;
         }
 
-        return $max;
+        if (($len1 + $len2) % 2 == 1) {
+            return $goalNum;
+        } else {
+            $nextNum = ($j >= $len2 || ($i < $len1 && $nums1[$i] < $nums2[$j])) ? $nums1[$i] : $nums2[$j];
+            return ($goalNum + $nextNum) / 2;
+        }
     }
 }
