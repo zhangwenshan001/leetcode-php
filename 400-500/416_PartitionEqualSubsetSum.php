0bd126a7c22ff01d13921faef1e04d9c15b43813
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-07 16:52:22 +0900

    add 416

---
+++ 400-500/416_PartitionEqualSubsetSum.php
@@ -0,0 +1,30 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    function canPartition($nums) {
+        $sum = 0;
+        foreach($nums as $num) {
+            $sum += $num;
+        }
+        
+        if ($sum % 2 == 1) {
+            return false;
+        }
+        
+        $sum = $sum / 2;
+    
+        $dp = array_pad([], $sum + 1, false);
+        $dp[0] = true;
+        foreach($nums as $num) {
+            for($j = $sum;$j>0;$j--) {
+                if ($j-$num >=0) {
+                    $dp[$j] = $dp[$j] || $dp[$j - $num];
+                }
+            }
+        }
+        return $dp[$sum];
+    }
+}
