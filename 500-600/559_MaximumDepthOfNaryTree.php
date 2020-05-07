7f7a6058dc4e93f8df78d97f51f8e0569821e59d
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-07 17:10:03 +0900

    Create 559_MaximumDepthOfNaryTree.php

---
+++ 500-600/559_MaximumDepthOfNaryTree.php
@@ -0,0 +1,38 @@
+/**
+ * Definition for a Node.
+ * class Node {
+ *     public $val = null;
+ *     public $children = null;
+ *     function __construct($val = 0) {
+ *         $this->val = $val;
+ *         $this->children = array();
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param Node $root
+     * @return integer
+     */
+    function maxDepth($root) {
+        return $this->maxDepthHandle($root, 0);
+    }
+    
+    function maxDepthHandle($root, $depth){
+        if ($root == null) {
+            return $depth;
+        }
+        
+        if ($root->children == null) {
+            return 1;
+        }
+        $max = 0;
+        foreach($root->children as $child) {
+            //var_dump($child);
+            $max = max($max,  $this->maxDepthHandle($child, $depth)+1); 
+        }
+  
+        return $max;
+    }
+}
