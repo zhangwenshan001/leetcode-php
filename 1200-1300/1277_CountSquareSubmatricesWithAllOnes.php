b6c7a59f5413e316e26af42cef28b43d8a882e06
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-07 17:26:08 +0900

    Create 1277_CountSquareSubmatricesWithAllOnes.php

---
+++ 1200-1300/1277_CountSquareSubmatricesWithAllOnes.php
@@ -0,0 +1,23 @@
+class Solution {
+
+    /**
+     * @param Integer[][] $matrix
+     * @return Integer
+     */
+    function countSquares($matrix) {
+        $sum = 0;
+        $m = count($matrix);
+        $n = count(current($matrix));
+        
+        for($i = 0;$i<$m;$i++) {
+            for($j = 0;$j<$n;$j++) {
+                if ($i > 0 && $j > 0 && $matrix[$i][$j] == 1) {
+                    $matrix[$i][$j] =  min($matrix[$i-1][$j], $matrix[$i][$j-1],$matrix[$i-1][$j-1])+1;
+                }
+                $sum = $sum + $matrix[$i][$j];
+            }
+        }
+        
+        return $sum;
+    }
+}
