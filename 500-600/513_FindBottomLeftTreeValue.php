94f83b30cf614b09da4a69962d69d9091484a761
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-08 11:39:20 +0900

    Create 513_FindBottomLeftTreeValue.php

---
+++ 500-600/513_FindBottomLeftTreeValue.php
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function findBottomLeftValue($root) {
+        if ($root == null) {
+            return null;
+        }
+        
+        $queue = [$root];
+        while(count($queue) != 0) {
+            
+            $res = $queue[0];
+            $count = count($queue);
+            for($i = 0;$i<$count;$i++) {
+                if ($queue[$i]->left != null) {
+                    $queue[] =  $queue[$i]->left;
+                }
+                 if ($queue[$i]->right != null) {
+                    $queue[] =  $queue[$i]->right;
+                }
+            }
+            for($i = 0;$i<$count;$i++) {
+                array_shift($queue);
+            }
+            
+        }
+       
+        return $res->val;
+    }
+}
