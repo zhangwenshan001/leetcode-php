aa582aa1383eaf23f8bdc0f0a4ad601d3c092f4d
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-08 14:05:43 +0900

    Create 1306_JumpGameIII.php

---
+++ 1300-1400/1306_JumpGameIII.php
@@ -0,0 +1,44 @@
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @param Integer $start
+     * @return Boolean
+     */
+    function canReach($arr, $start) {
+        if ($arr[$start] == 0) {
+            return true;
+        }
+        $map = [];
+        $next = [$start];
+        
+        while(!empty($next)) {
+            $cur = current($next);
+            
+            if (!empty($map[$cur])) {
+                array_shift($next);
+                continue;
+            }
+            
+            $map[$cur] = 1;
+            if ($cur - $arr[$cur] >= 0) {
+                if ($arr[$cur - $arr[$cur]] == 0) {
+                    return true;
+                } else {
+                    $next[] = $cur - $arr[$cur];
+                }
+            }
+            
+            if ($cur + $arr[$cur] <count($arr)) {
+                if ($arr[$cur + $arr[$cur]] == 0) {
+                    return true;
+                } else {
+                    $next[] = $cur + $arr[$cur];
+                }
+            }
+            array_shift($next);
+                
+        }
+        return false;
+    }
+}
