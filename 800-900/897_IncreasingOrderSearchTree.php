41ab0873ad3c579f82dbd6a9e642f152f17e7dc5
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-08 11:26:46 +0900

    Create 897_IncreasingOrderSearchTree.php

---
+++ 800-900/897_IncreasingOrderSearchTree.php
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return TreeNode
+     */
+    function increasingBST($root) {
+        $res = $this->increasingBSTHandle($root);
+        return $res[0];
+    }
+    
+    function increasingBSTHandle($root) {
+        if ($root == null) {
+            return null;
+        }
+        
+        $newRoot = $root;
+        $newTail = $root;
+
+       
+        if ($root->right != null) {
+            [$rightRoot, $rightTail] = $this->increasingBSTHandle($root->right);
+            $root->right = $rightRoot;
+            $newTail = $rightTail;
+        }
+        
+        if ($root->left != null) {
+            [$leftRoot, $leftTail] = $this->increasingBSTHandle($root->left);
+            $leftTail->right = $root;
+            $newRoot = $leftRoot;
+            $root->left = null;
+        } 
+        
+        return [$newRoot, $newTail];
+    }
+}
