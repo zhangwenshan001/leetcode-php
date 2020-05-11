e6e9a0663868759685a4a831d3229803f7a94c96
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-11 09:39:29 +0900

    Create 997_FindTheTownJudge.php

---
+++ 900-1000/997_FindTheTownJudge.php
@@ -0,0 +1,33 @@
+class Solution {
+
+    /**
+     * @param Integer $N
+     * @param Integer[][] $trust
+     * @return Integer
+     */
+    function findJudge($N, $trust) {
+        if ($N == 1) {
+            return $N;
+        }
+        $trusted = [];
+        $trusting = [];
+        for($i = 0;$i<count($trust);$i++) {
+            if (!empty($trusted[$trust[$i][1]])) {
+                $trusted[$trust[$i][1]]++;
+            }
+            else {
+                $trusted[$trust[$i][1]] = 1;
+            }
+            
+            $trusting[$trust[$i][0]] = 1;
+        }
+      
+        for($i = 1;$i<=$N;$i++) {  
+            if (!empty($trusted[$i]) && $trusted[$i] == $N-1 && empty($trusting[$i])) {
+                return $i;
+            }
+        }
+        
+        return -1;
+    }
+}
