b41944753963aaa1c70db924a12ed8fb0566e69d
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-12 09:48:03 +0900

    Create 983_MinimumCostForTickets.php

---
+++ 900-1000/983_MinimumCostForTickets.php
@@ -0,0 +1,31 @@
+class Solution {
+
+    /**
+     * @param Integer[] $days
+     * @param Integer[] $costs
+     * @return Integer
+     */
+    function mincostTickets($days, $costs) {
+        $dayCount = count($days);
+        $dp = [];
+        
+        $dp[$dayCount] = 0;
+        $dp[$dayCount - 1] = min($costs);
+        
+        for($i = $dayCount-2;$i>=0;$i--) {
+            $min = $dp[$i+1] + $costs[0];
+            $j = $i + 1;
+            while($j < $dayCount && $days[$j] - $days[$i] < 7) {
+                $j++;
+            }
+            $min = min($min, $dp[$j] + $costs[1]);
+            while($j < $dayCount && $days[$j] - $days[$i] < 30) {
+                $j++;
+            }
+            $min = min($min, $dp[$j] + $costs[2]);
+            $dp[$i] = $min;
+        }
+        
+        return $dp[0];
+    }
+}
