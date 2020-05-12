67485684b5eb55380f4b3417399adcafd092f2dd
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-12 09:57:52 +0900

    Create 1302_DeepestLeavesSum.php

---
+++ 1300-1400/1302_DeepestLeavesSum.php
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private $depths = [];
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function deepestLeavesSum($root) {
+        $this->deepestLeavesSumHandle($root, 0);
+        
+        if (count($this->depths) == 0) {
+            return 0;
+        }
+        return $this->depths[count($this->depths)-1];
+    }
+    
+    function deepestLeavesSumHandle($root, $depth) {
+        if ($root == null) {
+            return;
+        }
+        if (empty($this->depths[$depth])) {
+            $this->depths[$depth] = $root->val;
+        } else {
+             $this->depths[$depth] = $this->depths[$depth] + $root->val;
+        }
+        $this->deepestLeavesSumHandle($root->left, $depth+1);
+        $this->deepestLeavesSumHandle($root->right, $depth+1);
+        
+        return;
+    }
+}
