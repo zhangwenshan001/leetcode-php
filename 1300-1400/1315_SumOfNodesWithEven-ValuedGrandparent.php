850120e80ae5a4545410dcd003a0534f966eaf13
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-13 09:43:21 +0900

    Create 1315_SumOfNodesWithEven-ValuedGrandparent.php

---
+++ 1300-1400/1315_SumOfNodesWithEven-ValuedGrandparent.php
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+    private $sum = 0;
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function sumEvenGrandparent($root) {
+        if ($root == null) {
+            return 0;
+        }
+        $this->dfs($root, false);
+        return $this->sum;
+    }
+    
+    function dfs($root, $parentIsEven) {
+        $isEven = ($root->val % 2 == 0);
+        if ($root->left != null) {
+            if ($parentIsEven) {
+                $this->sum += $root->left->val;
+            }
+            $this->dfs($root->left, $isEven);
+        }
+        if ($root->right != null) {
+            if ($parentIsEven) {
+                $this->sum += $root->right->val;
+            }
+            $this->dfs($root->right, $isEven);
+        }
+        return;
+    }
+}
