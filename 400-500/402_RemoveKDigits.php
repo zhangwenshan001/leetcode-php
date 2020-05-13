0680c1396f82a46d9ee1a0d08595e429d13edf8e
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-13 17:48:45 +0900

    Create 402_RemoveKDigits.php

---
+++ 400-500/402_RemoveKDigits.php
@@ -0,0 +1,40 @@
+class Solution {
+
+    /**
+     * @param String $num
+     * @param Integer $k
+     * @return String
+     */
+    function removeKdigits($num, $k) {
+        if (strlen($num) <= $k) {
+            return "0";
+        }
+        
+        $res=[];
+        
+        $n = strlen($num);
+        for($i = 0;$i<$n;$i++){
+            while(!empty($res) && end($res) > $num[$i] && $k > 0) {
+                array_pop($res);
+                $k--;
+                if ($k == 0) {
+                    break;
+                }
+            }
+            
+            array_push($res, $num[$i]);
+        }
+        
+        $i = 0;
+        $l = count($res);
+        for(;$i<$l;$i++) {
+            if ($res[$i] != "0") {
+                break;
+            }
+        }
+        if ($i == $l) {
+            return "0";
+        }
+        return substr(implode("", $res), $i, $l-$k-$i);
+    }
+}
