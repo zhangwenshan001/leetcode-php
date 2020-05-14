57e5d45592c38f0c3ecbae702194fbe89ab4f662
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-14 10:24:29 +0900

    Create 712_MinimumASCIIDeleteSumForTwoStrings.php

---
+++ 700-800/712_MinimumASCIIDeleteSumForTwoStrings.php
@@ -0,0 +1,34 @@
+class Solution {
+
+    private $dp = [];
+    
+    /**
+     * @param String $s1
+     * @param String $s2
+     * @return Integer
+     */
+    function minimumDeleteSum($s1, $s2) {
+        return $this->handle($s1,$s2,0,0);
+    }
+    
+    function handle($s1, $s2, $i, $j) {
+        if(!empty($this->dp[$i][$j])) {
+            return $this->dp[$i][$j];
+        }
+        
+        if ($i == strlen($s1) && $j == strlen($s2)) {
+            return 0;
+        } else if ($i == strlen($s1)) {
+            $this->dp[$i][$j] = ord($s2[$j]) + $this->handle($s1, $s2, $i, $j+1);
+        } else if ($j == strlen($s2)) {
+            $this->dp[$i][$j] = ord($s1[$i]) + $this->handle($s1, $s2, $i+1, $j);
+        } else {
+            if ($s1[$i] == $s2[$j]) {
+                $this->dp[$i][$j] = $this->handle($s1, $s2, $i+1, $j+1);
+            } else {
+                $this->dp[$i][$j] = min(ord($s2[$j]) + $this->handle($s1,$s2, $i, $j+1), ord($s1[$i]) + $this->handle($s1, $s2, $i+1, $j));
+            }
+        }  
+        return $this->dp[$i][$j];
+    }
+}
