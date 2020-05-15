14b1dd5a277484b79c184ef1e938127c039dd0d6
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-15 10:17:03 +0900

    Create 300_LongestIncreasingSubsequence.php

---
+++ 200-300/300_LongestIncreasingSubsequence.php
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function lengthOfLIS($nums) {
+        if ($nums == null || count($nums) == 0) {
+            return 0;
+        }
+        $dp = [];
+        
+        $n = count($nums);
+        $dp[0] = 1;
+        $max = 1;
+        for($i = 1;$i<$n;$i++) {
+            $dp[$i] = PHP_INT_MIN;
+            for($j = 0;$j<$i;$j++) {
+                $cur = ($nums[$i]>$nums[$j]) ? $dp[$j] + 1: 1;
+                $dp[$i] = max($dp[$i], $cur);
+            }
+            $max = max($max, $dp[$i]);
+        }
+       
+        return $max;
+        
+    }
+}
