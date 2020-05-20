f61360a363eedd224c4ea49a8209931154639392
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-20 13:22:05 +0900

    Create 901_OnlineStockSpan.php

---
+++ 900-1000/901_OnlineStockSpan.php
@@ -0,0 +1,44 @@
+class StockSpanner {
+    
+    private $prices;
+    private $memo;
+    
+    function __construct() {
+        $this->prices = [];
+        $this->memo = [];
+    }
+  
+    /**
+     * @param Integer $price
+     * @return Integer
+     */
+    function next($price) {
+        
+        $this->prices[] = $price;
+        $i = count($this->prices)-1;
+        
+        $j = $i-1;
+        $count = 1;
+        while($j >= 0) {
+            if ($this->prices[$i] >= $this->prices[$j]) {
+                if ($this->memo[$j] == 1) {
+                    $count++;
+                    $j = $j-1;
+                } else {
+                    $count += $this->memo[$j];
+                    $j = $j - $this->memo[$j];
+                }
+            } else {      
+                break;     
+            }
+        }
+        $this->memo[$i] = $count;
+        return $count;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * $obj = StockSpanner();
+ * $ret_1 = $obj->next($price);
+ */
