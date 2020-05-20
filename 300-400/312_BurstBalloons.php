95557af5aa55ce7215bb68f12bed72f311a64ac2
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-20 15:56:44 +0900

    Create 312_BurstBalloons.php

---
+++ 300-400/312_BurstBalloons.php
@@ -0,0 +1,35 @@
+class Solution {
+
+    private $memo = [];
+    private $nums;
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function maxCoins($nums) {
+        $c = count($nums);
+        $nums[-1] = 1;
+        $nums[$c] = 1;
+        $this->nums = $nums;
+        
+        return $this->burst(-1, $c);
+    }
+    
+    function burst($left, $right) {
+        if ($left + 1 == $right) {
+            return 0;
+        }
+        if (!empty($this->memo[$left][$right])) {
+            return $this->memo[$left][$right];
+        }
+        
+        $cur = 0;
+        for($i = $left + 1;$i<$right;$i++) {
+            $cur = max($cur, $this->nums[$left] * $this->nums[$i] * $this->nums[$right] + 
+                      $this->burst($left, $i) + $this->burst($i,$right));
+        }
+        $this->memo[$left][$right] = $cur;
+        
+        return $cur;
+    }
+}
