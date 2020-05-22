3111ec237b70d48528f266cf367ee7c32fc11684
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-22 10:59:49 +0900

    Create 328_OddEvenLinkedList.php

---
+++ 300-400/328_OddEvenLinkedList.php
@@ -0,0 +1,40 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function oddEvenList($head) {
+        if ($head == null || $head->next == null) {
+            return $head;
+        }
+        $oddHead = $head;
+        $evenHead = $head->next;
+        
+        $p1 = $oddHead;
+        $p2 = $evenHead;
+        
+        while($p2!=null && $p2->next !=null) {
+            $nextP1 = $p2->next;
+            $p1->next = $nextP1;
+            $p2->next = $nextP1->next;
+            $p1 = $p1->next;
+            $p2 = $p2->next;
+        }
+        
+        $p1->next = $evenHead;
+        
+        return $oddHead;
+    }
+}
