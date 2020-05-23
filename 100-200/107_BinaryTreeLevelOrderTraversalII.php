3e4515418c01d99a9989e6e587d93d6b611ab6f3
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-23 15:19:04 +0900

    Create 107_BinaryTreeLevelOrderTraversalII.php

---
+++ 100-200/107_BinaryTreeLevelOrderTraversalII.php
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[][]
+     */
+    function levelOrderBottom($root) {
+        if ($root == null) {
+            return [];
+        }
+        $res = [];
+        $queue = [$root];
+        while($count = count($queue)) {
+
+
+            for ($i = 0;$i < $count;$i++) {
+                if ($queue[$i]->left != null) {
+                    $queue[] = $queue[$i]->left;
+                }
+                if ($queue[$i]->right != null) {
+                    $queue[] = $queue[$i]->right;
+                }
+            }
+            $cur = [];
+            for ($i =0;$i<$count;$i++) {
+                $cur[] = $queue[0]->val;
+                array_shift($queue);
+            }
+            array_unshift($res, $cur);
+        }
+        return $res;
+    }
+}
