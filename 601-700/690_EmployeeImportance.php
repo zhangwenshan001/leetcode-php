46cd22cc909a3e39a1b5962d36576a471e26a19c
zhangwenshan001 <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-23 15:21:45 +0900

    Create 690_EmployeeImportance.php

---
+++ 601-700/690_EmployeeImportance.php
@@ -0,0 +1,50 @@
+/**
+ * Definition for Employee.
+ * class Employee {
+ *     public $id = null;
+ *     public $importance = null;
+ *     public $subordinates = array();
+ *     function __construct($id, $importance, $subordinates) {
+ *         $this->id = $id;
+ *         $this->importance = $importance;
+ *         $this->subordinates = $subordinates;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param Employee[] $employees
+     * @param Integer $id
+     * @return Integer
+     */
+    function getImportance($employees, $id) {
+        $mark = [];
+        $map = [];
+        foreach ($employees as $employee) {
+            $mark[$employee->id] = false;
+            $map[$employee->id] = $employee;
+        }
+
+        $queue = [$id];
+        $importance = 0;
+
+        while(count($queue)!=0) {
+            $count = count($queue);
+            for($i = 0;$i<$count;$i++) {
+                $importance = $importance + $map[$queue[$i]]->importance;
+                $mark[$queue[$i]] = true;
+                foreach($map[$queue[$i]]->subordinates as $subid) {
+                    if ($mark[$subid] != true) {
+                        $queue[] = $subid;
+                    }
+                }
+            }
+            for($i = 0;$i<$count;$i++) {
+                array_shift($queue);
+            }
+        }
+
+        return $importance;
+    }
+}
