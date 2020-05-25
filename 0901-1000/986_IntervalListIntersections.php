00dc1898b8ea14753fdeec20d3c1155e997fb78e
zhangws <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-25 10:03:43 +0900

    Create 986_IntervalListIntersections.php

---
+++ 0901-1000/986_IntervalListIntersections.php
@@ -0,0 +1,51 @@
+class Solution {
+
+    /**
+     * @param Integer[][] $A
+     * @param Integer[][] $B
+     * @return Integer[][]
+     */
+    function intervalIntersection($A, $B) {
+        if (count($A) == 0 || count($B) == 0) {
+            return [];
+        }
+        $res = [];
+        $i = 0;
+        $j = 0;
+        $curA = $A[$i];
+        $curB = $B[$j];
+        
+        $countA = count($A);
+        $countB = count($B);
+        
+        while(true) {
+            if ($curA[0] >= $curB[0] && $curA[0] <= $curB[1] && $curB[1] <= $curA[1]) {
+                $res[] = [$curA[0], $curB[1]];
+                $j++;
+            } else if ($curB[0] >= $curA[0] && $curB[0] <= $curA[1] && $curA[1] <= $curB[1]) {
+                $res[] = [$curB[0], $curA[1]];
+                $i++;
+            } else if ($curA[0] >= $curB[0] && $curA[1] <= $curB[1]) {
+                $res[] = $curA;
+                $i++;
+            } else if ($curB[0] >= $curA[0] && $curB[1] <= $curA[1]) {
+                $res[] = $curB;
+                $j++;
+            } else {
+                if ($curB[1] > $curA[1]) {
+                    $i++;
+                } else {
+                    $j++;
+                }
+            }
+            
+            if ($i == $countA || $j == $countB) {
+                break;
+            }
+            $curA = $A[$i];
+            $curB = $B[$j];
+        }
+        
+        return $res;
+    }
+}
