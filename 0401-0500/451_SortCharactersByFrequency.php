15373f261bb2d2c42509f727458b889f32fa9dd1
zhangws <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-25 09:24:54 +0900

    Create 451_SortCharactersByFrequency.php

---
+++ 0401-0500/451_SortCharactersByFrequency.php
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function frequencySort($s) {
+        $len = strlen($s);
+        $static = [];
+        for($i=0;$i<$len;$i++) {
+            if (empty($static[$s[$i]])) {
+                $static[$s[$i]] = 1; 
+            }else {
+                $static[$s[$i]]++; 
+            }    
+        }
+        
+        asort($static);
+        $count = count($static);
+        $res = '';
+        foreach($static as $key => $value) {
+            $res = str_pad("", $value, $key) . $res;
+        }
+        
+        return $res;
+        
+    }
+}
