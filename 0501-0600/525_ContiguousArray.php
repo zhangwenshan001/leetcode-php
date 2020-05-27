f1dfef03287f260631b55d25301549524afe388c
zhangws <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-27 15:57:31 +0900

    Create 525_ContiguousArray.php

---
+++ 0501-0600/525_ContiguousArray.php
@@ -0,0 +1,33 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findMaxLength($nums) {
+        if (count($nums) <= 1) {
+            return 0;
+        }
+        
+        $map = [];
+        $map[0] = 0;
+        $c = count($nums);
+        $max = 0;
+        $curKey = 0;
+        for($i = 1;$i<=$c;$i++) {
+            if ($nums[$i-1] == 0) {
+                $curKey--;
+            } else {
+                $curKey++;
+            }
+            
+            if (!isset($map[$curKey])) {
+                $map[$curKey] = $i;
+            } else {
+                $max = max($max, $i - $map[$curKey]);
+            }
+        }
+        
+        return $max;
+    }
+}
