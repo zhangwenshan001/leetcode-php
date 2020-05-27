ab52dac89fd989066183012eb6bf75b9b822da68
zhangws <58196852+zhangwenshan001@users.noreply.github.com>
2020-05-27 17:28:12 +0900

    Create 207_CourseSchedule.php

---
+++ 0201-0300/207_CourseSchedule.php
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param Integer $numCourses
+     * @param Integer[][] $prerequisites
+     * @return Boolean
+     */
+    function canFinish($numCourses, $prerequisites) {
+        $map = array_fill(0, $numCourses, []);
+        $indegree = array_fill(0, $numCourses, 0); //course has how many pre courses;
+        
+        foreach($prerequisites as $pre) {
+            if (!in_array($pre[0], $map[$pre[1]])) {
+                $indegree[$pre[0]]++;  
+            }
+            $map[$pre[1]][] = $pre[0];
+        }
+    
+        $count = 0;
+        $queue = [];
+        
+        foreach($indegree as $key => $value) { 
+            if ($value == 0) {
+                $queue[] = $key; //the course which has no pre courses;
+            }
+        }
+        while(count($queue) != 0) {
+            $course = $queue[0];
+            array_shift($queue);
+            $count++;
+            foreach($map[$course] as $key => $value) {
+                if (--$indegree[$value] ==0) {
+                    $queue[] = $value;
+                }    
+            }
+        }
+        
+        return $count == $numCourses;
+    }
+}
