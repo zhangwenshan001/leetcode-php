2ff618673abeef20ea98f1b90fd676495b3a35e8
zhangws <58196852+zhangwenshan001@users.noreply.github.com>
2020-06-01 11:18:07 +0900

    Create 973_KClosestPointsToOrigin.php

---
+++ 0901-1000/973_KClosestPointsToOrigin.php
@@ -0,0 +1,32 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[][] $points
+     * @param Integer $K
+     * @return Integer[][]
+     */
+    function kClosest($points, $K) {
+        $dists = [];
+        foreach($points as $key => $point) {
+            $dist = $point[0] * $point[0] + $point[1] * $point[1];
+            $dists[$key] = $dist;
+        }
+        
+        asort($dists);
+        $res = [];
+        
+        $count = 0;
+        foreach($dists as $index => $dist) {
+            $res[] = $points[$index];
+            $count++;
+            if ($count == $K) {
+                break;
+            }
+        }
+        
+        
+        return $res;
+    }
+}
