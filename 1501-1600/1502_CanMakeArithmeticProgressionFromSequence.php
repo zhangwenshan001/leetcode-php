aa0bf1b9e37606aa1b43aac96f39c10241ac9c1c
zhangws <58196852+zhangwenshan001@users.noreply.github.com>
2020-07-06 13:39:08 +0900

    Create 1502_CanMakeArithmeticProgressionFromSequence.php

---
+++ 1501-1600/1502_CanMakeArithmeticProgressionFromSequence.php
@@ -0,0 +1,20 @@
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Boolean
+     */
+    function canMakeArithmeticProgression($arr) {
+        sort($arr);
+        $d = $arr[1] - $arr[0];
+        $n = count($arr);
+        for($i = 2;$i<$n;$i++) {
+            if ($arr[$i] - $arr[$i-1] != $d) {
+                return false;
+            }
+        }
+        
+        return true;
+            
+    }
+}
