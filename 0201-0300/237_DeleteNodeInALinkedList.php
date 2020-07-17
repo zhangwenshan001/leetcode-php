749099b5f89991c28c3002ef0bf06a5846927863
zhangws <58196852+zhangwenshan001@users.noreply.github.com>
2020-07-17 14:38:05 +0900

    Create 237_DeleteNodeInALinkedList.php

---
+++ 0201-0300/237_DeleteNodeInALinkedList.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param ListNode $node
+     * @return 
+     */
+    function deleteNode($node) {
+        if ($node == null || $node->next == null) {
+            return;
+        }
+        
+        $node->val = $node->next->val;
+        $node->next = $node->next->next;
+        
+        return;
+    }
+}
